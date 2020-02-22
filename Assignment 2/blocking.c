9d36ad98a63f29456c205209c94db029e46bb422
Crekkaran <cs5170411@iitd.ac.in>
2020-02-23 04:45:21 +0530

    cont

--- Assignment 2/blocking.c
+++ Assignment 2/blocking.c
@@ -63,17 +63,33 @@ int main(int argc, char const *argv[])
 		// printMatrix(C, M, M);
 		
 		for(int f=0;f<num_processes-1;f++){
-			// printf("YOLO\n");
-			MPI_Rsend(A, (int)(N*M/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*13, comm);
-			MPI_Rsend(B, (int)(M*N/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*97, comm);
-			
-			// int m_line = 0;
-			// for(int k=0;k<N*M/(num_processes-1);k++){
-			// 	MPI_Rsend(A[], )
-			// }
-			for(int k=0;k<N;k++){
-				MPI_Rsend(A[f*(M/(num_processes-1)) + k*M], (int)(M/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*13, comm);
+			A_block = malloc_matrix(N, M/(num_processes-1));
+			B_block = malloc_matrix(M/(num_processes-1), N);
+
+			for(int i=0;i<N*M/2;i++){
+				B_block[i] = B[i + (N*M*f)/2];
 			}
+
+			for(int i=0;i<N*M/2;i+=(M/(num_processes-1))){
+				for(int j=0;j<M/(num_processes-1);j++){
+					A_block[i+j] = A[i+j + (N*M*f)/2];
+				}
+			}
+
+			int counter = 0;
+			for(int i=0;i<N*M/2;i+=2){
+				A_block[counter++] = A[i];
+				A_block[counter++] = A[i+1];
+			}
+
+			MPI_Rsend(A_block, (int)(N*M/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*13, comm);
+			// MPI_Rsend(B_block, (int)(M*N/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*97, comm);
+			
+			
+
+			// for(int k=0;k<N;k++){
+			// 	MPI_Rsend(A[f*(M/(num_processes-1)) + k*M], (int)(M/(num_processes-1)), MPI_FLOAT, f+1, (f+1)*13, comm);
+			// }
 		}
 	}
 	else if (rank!=0)
@@ -85,10 +101,10 @@ int main(int argc, char const *argv[])
 
 		MPI_Status status;
 		MPI_Recv(A_block, INT_MAX, MPI_FLOAT, 0, (rank)*13, comm, &status);
-		MPI_Recv(B_block, INT_MAX, MPI_FLOAT, 0, (rank)*97, comm, &status);
+		// MPI_Recv(B_block, INT_MAX, MPI_FLOAT, 0, (rank)*97, comm, &status);
 		printf("====================== Process %d ===================\n", rank);
-		printMatrix(A_block, N/(num_processes-1), M);
-		printMatrix(B_block, M, N/(num_processes-1));
+		printMatrix(A_block, N, M/(num_processes-1));
+		// printMatrix(B_block, M/(num_processes-1), N);
 		printf("\n");
 
 	}
